50d5eb21e9abea56aa3263c8ef977e4c0bdc2279
Abdulhamid <abdulhamidhersi@gmail.com>
2024-09-17 14:46:52 +0300

    complete

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 
-function distanceFromHqInBlocks(someValue) {
-    return Math.abs(someValue - 42);
-  }
   
+ function distanceFromHqInBlocks(street) {
+   // return (street - 42);
+   const blocksFromHq = Math.abs(42 -street)
+   return blocksFromHq
+  }
 
-  function distanceFromHqInFeet(someValue) {
-    let blocks = distanceFromHqInBlocks(someValue);
-    return blocks * 264; 
+  function distanceFromHqInFeet(location) {
+    const blocks = distanceFromHqInBlocks(location);
+    return blocks * 264;
   }
-  
-  
+
   function distanceTravelledInFeet(start, destination) {
-    let blocksTravelled = Math.abs(destination - start);
-    return blocksTravelled * 264; 
+    const blocksTravelled = Math.abs(destination - start);
+    return blocksTravelled* 264;
   }
-  
-  
-  function calculatesFarePrice(start, destination) {
-    const distance = distanceTravelledInFeet(start, destination);
-  
-    if (distance <= 400) {
-      return 0;
-    } else if (distance > 400 && distance <= 2000) {
-      return (distance - 400) * 0.02;
-    } else if (distance > 2000 && distance <= 2500) {
-      return 25;
-    } else {
-      return 'cannot travel that far';
+  function calculatesFarePrice(start,destination) {
+    const feetTravelled = distanceTravelledInFeet(start,destination);
+    let fare;
+    if (feetTravelled <= 400) {
+      fare = 0;
+    }  else if (feetTravelled<=2000) { 
+      fare = (feetTravelled - 400) * 0.02;
+    } else if (feetTravelled <= 2500) {
+      fare = 25;
     }
+      else if (feetTravelled > 2500) {
+        fare = 'cannot travel that far';
+      
+    }
+    return fare
   }
-  
- 
